d3c00fcc37f1bd675823f43d7727ca1c2e965579
Alex Chan <alex@alexwlchan.net>
2023-10-12 09:08:48 +0100

    Remove my now-deleted wellcome-scripts from my PATH

--- config.fish
+++ config.fish
@@ -20,7 +20,6 @@ fish_add_path ~/repos/scripts/macos
 fish_add_path ~/repos/scripts/python
 fish_add_path ~/repos/scripts/terraform
 fish_add_path ~/repos/scripts/text
-fish_add_path ~/repos/scripts/wellcome
 fish_add_path ~/repos/private-scripts
 
 fish_add_path ~/.cargo/bin
